e73388b44b1e5d09a6d017bce4f579e0b029c854
ukind <ukind@users.noreply.github.com>
2018-02-06 13:03:43 +0700

    DRUMKIT IN JAVASCRIPT

---
+++ 1/js/index.js
@@ -0,0 +1,30 @@
+
+var removeTransition = (e) => e.target.classList.remove('playing');
+
+//oldSchool
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(function(element, index, array) {
+  element.addEventListener('transitionend', removeTransition);
+});
+
+// ES6 Version
+// const keys = Array.from(document.querySelectorAll('.key'));
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-audio-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) {
+    return;
+  }
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+//OLDSCHOOL
+// function removeTransition(e) {
+//   e.target.classList.remove('playing');
+// }

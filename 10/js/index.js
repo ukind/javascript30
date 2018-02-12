58df67044e17b1a8822aa4272dbe587f9876419c
ukind <ukind@users.noreply.github.com>
2018-02-12 16:03:28 +0700

    multiple checkbox

---
+++ 10/js/index.js
@@ -0,0 +1,28 @@
+const allCheckBox = document.querySelectorAll('.todoListItem input[type="checkbox"]');
+
+let lastChecked;
+
+
+function handleCheck(e) {
+  let isInBetween = false;
+  if (e.shiftKey && this.checked) {
+    // do loop for every checkedbox
+    allCheckBox.forEach(checkBox => {
+      //will log all chechbox
+      //if allCheckBox === this(the selected checbox) or
+      if (checkBox === this || checkBox === lastChecked) {
+        isInBetween = !isInBetween;
+        console.log('check in between');
+      }
+      
+      if (isInBetween) {
+        checkBox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+allCheckBox.forEach(thisCheckbox => {
+  thisCheckbox.addEventListener('click', handleCheck);
+});

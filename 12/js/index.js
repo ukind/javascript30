38850ab6bbcc56df979dd433d3894ed00c1aa471
ukind <ukind@users.noreply.github.com>
2018-02-28 12:09:57 +0700

    mantap permirsa: multiple cheatcode

--- 12/js/index.js
+++ 12/js/index.js
@@ -1,5 +1,7 @@
-const theSecretKey = 'ukind';
+const theSecretKey = 'aku';
 const pressedKey = [];
+
+const kumpulanPassKey = ['ukind', 'aku'];
 
 window.addEventListener('keyup', event => {
   console.log(event);
@@ -10,12 +12,24 @@ window.addEventListener('keyup', event => {
   // if inputer from array, negative. it start counting (for starting)
   // from right to left, and if positive, from left to right
   //              (-5-1) = -6 > char d at index 0. n at -1
-  console.table(theSecretKey);
+
+  // console.table(kumpulanPassKey);
   pressedKey.slice(-theSecretKey.length - 1,
     pressedKey.length - theSecretKey.length);
   // do calculate: -6, ex:27-5 = 21 => slice(-6, 21)
   // means: at position -6 remove (to right) 21 items (position -6 included in 21 item)
-  if (pressedKey.join('').includes(theSecretKey)) {
+  const test = pressedKey.join('');
+  console.log(test);
+
+  function findingPassCode(thePassKey) {
+    return kumpulanPassKey.find(elemen => {
+      if (thePassKey.includes(elemen)) {
+        return elemen;
+      }
+    });
+  }
+
+  if (pressedKey.join('').includes(findingPassCode(test))) {
     console.log('ding - ding');
     const sumArray = pressedKey.length;
     for (var i = 0; i < sumArray; i++) {

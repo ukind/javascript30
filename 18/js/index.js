f98992ac9e390ac98893dbd3e2be61cac9febea0
ukind <ukind@users.noreply.github.com>
2018-04-17 08:21:33 +0700

    refactoring

--- 18/js/index.js
+++ 18/js/index.js
@@ -1,29 +1,64 @@
+
 const timeSequence = Array.from(document.querySelectorAll('li'));
 
-const raw = timeSequence.map(node => node.dataset.time);
-console.log(raw);
-// create new array, with result of calling a provided function on every element in the calling array.
-const second = raw.map(separator => {
-  // parseFloat -> convert to number
-  const [menit, detik] = separator.split(':').map(parseFloat);
-  const calculated = (menit * 60) + detik;
-  return calculated;
-});
+function getTimeDataSet(time) {
+  return time.map(node => {
+    return node.dataset.time;
+  });
+}
+const rawTime = getTimeDataSet(timeSequence);
+function timeSeparator(rawTime) {
+  const menit = rawTime.map(element => {
+    const result = element.split(':');
+    return parseFloat(result[0]);
+  });
+  const detik = rawTime.map(element => {
+    const result = element.split(':');
+    return parseFloat(result[1]);
+  });
 
-// create method for summing all array value. by using reduce
-function pengakumulasi(akumulator, currentValue) {
-  console.log(akumulator + '+' + currentValue);
-  return akumulator + currentValue;
+  return {
+    'menit': menit,
+    'detik': detik
+  };
+};
+let timeSeparated = timeSeparator(rawTime);
+let totalTime = sumDuration(timeSeparated);
+
+function sumDuration(time) {
+  let detik = time.detik;
+  let menit = time.menit;
+  let sumDetik = 0;
+  let sumMenit = 0;
+
+  for (var i = 0; i < detik.length; i++) {
+    sumDetik += detik[i];
+  }
+  for (var i = 0; i < menit.length; i++) {
+    sumMenit += menit[i];
+  }
+  let totalDurationSecond = (sumDetik + (sumMenit * 60));
+  return {
+    'totalDurationSecond': totalDurationSecond
+  };
 }
 
-const totalDuration = second.reduce(pengakumulasi);
+const totalDuration = secondsToHms(totalTime.totalDurationSecond);
 
-let detikTersisa = totalDuration;
-// dibulatkan kebawah: math.floor: 4
-const jam = Math.floor(detikTersisa / 3600);
-detikTersisa = detikTersisa % 3600;
-console.log(detikTersisa);
+function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
 
-const menit = Math.floor(detikTersisa / 60);
-detikTersisa = detikTersisa % 60;
-console.log('Total durasi Video: ' + jam, menit, detikTersisa);
+  var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
+  var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
+  var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
+  return {
+    'Hour': hDisplay,
+    'Minutes': mDisplay,
+    'Second': sDisplay
+  };
+}
+
+console.log(`Total durasi video: ${totalDuration.Hour} ${totalDuration.Minutes} ${totalDuration.Second}`);
